deee2b5ab36c1a1d3a75e863e6c4f2e231240a4d
SamuelChen <samuelc254@nycstudents.net>
2026-05-26 10:32:45 -0400

    Added Window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I");
+        JFrame frame = new JFrame("game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 560);
+        frame.setLocationRelativeTo(null);
+
+        DisplayPanel panel = new DisplayPanel();
+
+        frame.add(panel);
+
+        frame.setVisible(true);
     }
 }

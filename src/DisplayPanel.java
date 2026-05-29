0ada319a98bb7269315f2220c0c1b94c2882bff6
XiangChengWen <xiangchengw@nycstudents.net>
2026-05-29 10:07:19 -0400

    images

---
+++ src/DisplayPanel.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DisplayPanel extends JPanel{
+    private int score;
+    private int steveX;
+    private int steveY;
+    private int zombieX;
+    private int zombieY;
+    private int skeletonX;
+    private int skeletonY;
+    private int creeperX;
+    private int creeperY;
+    private int bigSlimeX;
+    private int bigSlimeY;
+    private int smallSlime1X;
+    private int smallSlime1Y;
+    private int smallSlime2X;
+    private int smallSlime2Y;
+
+
+
+
+
+
+    public DisplayPanel() {
+        setBackground(Color.BLACK);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I");
+        JFrame frame = new JFrame("game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int width = 19 * 32; //19 col * 32 pixels
+        int height = 21 * 32; //21 row * 32 pixels
+
+        frame.setSize(width, height);
+        frame.setLocationRelativeTo(null);
+
+        DisplayPanel panel = new DisplayPanel();
+
+        frame.add(panel);
+
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }

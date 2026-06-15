d81c197c74b0bb9ead8e31a7667b981133248aed
Xiangchengw <Xiangchengw@nycstudents.net>
2026-06-15 10:32:12 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        int width = 1480;
-        int height = 1025; 
-
+        int width = 1496; //19 col * 32 pixels
+        int height = 1066; //21 row * 32 pixels
+        
         frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
-
+        
         DisplayPanel panel = new DisplayPanel();
-
+        
         frame.add(panel);
-
+        
         frame.setResizable(false);
         frame.setVisible(true);
     }

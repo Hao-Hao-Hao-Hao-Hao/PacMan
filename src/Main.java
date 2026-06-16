9b4cca338f2c60da4cc4b3eb81b64d1dba1eb397
SamuelChen254 <samuelc254@nycstudents.net>
2026-06-15 21:55:43 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,11 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("game");
+        DisplayPanel board = new DisplayPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        int width = 1496; //19 col * 32 pixels
-        int height = 1066; //21 row * 32 pixels
-        
-        frame.setSize(width, height);
+        frame.setSize(1500, 1120);
+        frame.add(board);
         frame.setLocationRelativeTo(null);
-        
-        DisplayPanel panel = new DisplayPanel();
-        
-        frame.add(panel);
-        
         frame.setResizable(false);
         frame.setVisible(true);
     }
